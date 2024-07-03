3a66dbdf7c6fd4efc7664052865a387db109f284
rasulbekkomilov <rasulbekkomilov7@gmail.com>
2024-07-03 15:48:23 +0500

    hammasi qoshildi

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import NavbarSection from './components/NavbarSection.vue'
+</script>
+
+<template>
+  <NavbarSection />
+</template>
+
+<style>
+
+</style>
---
+++ src/components/NavbarSection.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+   <h1 class="text">rasulbek</h1>   
+</template>
+
+<style lang="scss">
+
+
+</style>
