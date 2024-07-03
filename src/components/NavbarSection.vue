2318365794e879866bb635c2e4296b504b158c2f
rasulbekkomilov <rasulbekkomilov7@gmail.com>
2024-07-03 16:43:12 +0500

    bootstrap ishlamadi

--- src/components/NavbarSection.vue
+++ src/components/NavbarSection.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+   <nav class="navbar navbar-expand-lg navbar-light bg-body-secondary">
       <a class="navbar-brand" href="#">Navbar</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
          <span class="navbar-toggler-icon"></span>
@@ -32,7 +32,7 @@
             <a class="nav-link disabled" href="#">Disabled</a>
             </li>
          </ul>
-         <form class="form-inline my-2 my-lg-0">
+         <form class="form-inline my-2 my-lg-0 d-flex">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
          </form>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './assets/sass/main.scss'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 
 createApp(App).mount('#app')
