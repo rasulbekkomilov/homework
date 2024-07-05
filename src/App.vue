0c986b3be67b15975c949e259ae2e34a3d397a77
rasulbekkomilov <rasulbekkomilov7@gmail.com>
2024-07-05 22:16:59 +0500

    fetc

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-import NavbarSection from './components/NavbarSection.vue'
+import NavbarSection from './components/NavbarSection.vue';
 import HeaderSection from './components/HeaderSection.vue';
 </script>
 
 <template>
   <NavbarSection />
   <HeaderSection />
+
 </template>
 
 <style>
