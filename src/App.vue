cf3ce631277f15f77ce818c5b83b8fcdd5d297dd
rasulbekkomilov <rasulbekkomilov7@gmail.com>
2024-07-05 19:48:24 +0500

    header vue qoshildi

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import NavbarSection from './components/NavbarSection.vue'
+import HeaderSection from './components/HeaderSection.vue';
 </script>
 
 <template>
   <NavbarSection />
+  <HeaderSection />
 </template>
 
 <style>
