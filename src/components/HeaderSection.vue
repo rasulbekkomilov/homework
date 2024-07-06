897d3666bd8269af6aabba1ef5749eff0b36f1dd
rasulbekkomilov <rasulbekkomilov7@gmail.com>
2024-07-06 11:04:38 +0500

    haeder tugadi

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -1,25 +1,36 @@
 <script setup>
-const API = 'https://cbu.uz/uz/arkhiv-kursov-valyut/json/'
-fetch(API)
-   .then(res => res.json())
-   .then((data) => {
-      console.log(data)
-      let dollar = document.querySelector('.header-about_title')
+// const API = 'https://cbu.uz/uz/arkhiv-kursov-valyut/json/'
+// fetch(API)
+//    .then(res => res.json())
+//    .then((data) => {
+//       console.log(data)
+//       let dollar = document.querySelector('.header-about_title')
 
-      dollar.innerText = data[0].Rate
-   })
-   .catch(() => {
-      console.log('xatolik bor')
-   }) 
+//       dollar.innerText = data[0].Rate
+//    })
+//    .catch(() => {
+//       console.log('xatolik bor')
+//    })
+// FetchAPI sinaldi
 </script>
 
 <template>
    <div class="container d-flex align-items-center">
       <div class="header-about w-50">
-         <h1 class="header-about_title"></h1>
-         <p></p>
+         <h1 class="header-about_title">Get Instant Loans</h1>
+         <p>Get instant loans with ease on your smartphone.</p>
+         <div class="d-flex w-50">
+            <div class="google-icon w-50">
+               <img src="@/assets/play-market-icon.png" alt="Play Market Icon">
+            </div>
+            <div class="appstore-icon w-50">
+               <img src="@/assets/appstore-icon.png" alt="AppStore Icon">
+            </div>
+         </div>
       </div>
-      <div class="header-image w-50"></div>
+      <div class="header-image w-50">
+         <img class="img-fluid" src="@/assets/header-img.png" alt="Header Image">
+      </div>
    </div>
 </template>
 
--- src/components/NavbarSection.vue
+++ src/components/NavbarSection.vue
@@ -19,7 +19,7 @@
 			<div class="collapse navbar-collapse mx-5" id="navbarSupportedContent">
 				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 				<li class="nav-item">
-					<a class="nav-link active" aria-current="page" href="#">Home</a>
+					<a class="nav-link active text-primary" aria-current="page" href="#">Home</a>
 				</li>
 				<li class="nav-item">
 					<a class="nav-link" href="#">Contact</a>
