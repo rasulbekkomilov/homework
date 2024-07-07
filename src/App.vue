5eb618923d59b442116c912902be4e2027a2c1ef
rasulbekkomilov <rasulbekkomilov7@gmail.com>
2024-07-07 15:47:11 +0500

    options qismi muvaffaqiyatli qoshildi

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import NavbarSection from './components/NavbarSection.vue';
 import HeaderSection from './components/HeaderSection.vue';
+import Options from './components/Options.vue'
 </script>
 
 <template>
   <NavbarSection />
   <HeaderSection />
+  <Options />
 </template>
 
 <style>
