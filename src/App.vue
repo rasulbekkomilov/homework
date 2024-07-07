4073d3ddac4ba5f0d8737b2681fc654241bbaba4
rasulbekkomilov <rasulbekkomilov7@gmail.com>
2024-07-07 16:15:42 +0500

    cards qoshildi

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
 import NavbarSection from './components/NavbarSection.vue';
 import HeaderSection from './components/HeaderSection.vue';
-import Options from './components/Options.vue'
+import Options from './components/Options.vue';
+import Card from './components/Cards.vue'
 </script>
 
 <template>
   <NavbarSection />
   <HeaderSection />
   <Options />
+  <Card />
 </template>
 
 <style>
--- src/components/Options.vue
+++ src/components/Options.vue
@@ -4,9 +4,37 @@
 
 <template>
 
+   <div class="container d-flex align-items-center">
+      <div class="header-image w-50">
+         <img class="img-fluid" src="@/assets/terms-img.png" alt="Header Image">
+      </div>
+      <div class="header-about w-50">
+         <h1 class="header-about_title text-lg-">Save on your own terms</h1>
+         <p class="header-about_paragraph">Savings give you financial options</p>
+         <div class="d-flex w-75 image-container justify-content-between">
+            <div class="google-icon w-50 img-fluid">
+               <img class="google-icon w-75 img-fluid" src="@/assets/play-market-icon.png" alt="Play Market Icon">
+            </div>
+            <div class="appstore-icon w-50 img-fluid">
+               <img class="appstore-icon w-75 img-fluid" src="@/assets/appstore-icon.png" alt="AppStore Icon">
+            </div>
+         </div>
+      </div>
+   </div>
 
 </template>
 
 <style scoped>
-
+@media (max-width: 700px) {
+   .image-container{
+      display: flex;
+      /* flex-direction: column; */
+   }
+   .header-about_title {
+      font-size: 17px;
+   }
+   .header-about_paragraph {
+      font-size: 9px;
+   }
+}
 </style>
