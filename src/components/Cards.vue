b3e6932c3f24ffbee9c3b141893b978439eb3f5b
rasulbekkomilov <rasulbekkomilov7@gmail.com>
2024-07-09 17:53:13 +0500

    card qoshildi

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,10 +1,35 @@
-<script>    
+<script>
+
 </script>
 
 <template>
-   
+   <div class="cards d-flex w-100 justify-content-around text-center">
+      <div class="card pt-5 align-items-center text-center" style="width: 18rem;">
+         <img class="card-img-top img-fluid w-25" src="@/assets/card-1.png" alt="Card image cap">
+         <div class="card-body align-items-center">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+         </div>
+      </div>
+      <div class="card pt-5 align-items-center text-center" style="width: 18rem;">
+         <img class="card-img-top img-fluid w-25" src="@/assets/card-2.png" alt="Card image cap">
+         <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+         </div>
+      </div>
+      <div class="card pt-5 d-flex align-items-center text-center" style="width: 18rem;">
+         <img class="card-img-top img-fluid w-25" src="@/assets/card-3.png" alt="Card image cap">
+         <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+         </div>
+      </div>
+   </div>
 </template>
 
 <style scoped>
-
 </style>
