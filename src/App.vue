71a63abe57f733817f5bdb12256d70b743d7f7b8
rasulbekkomilov <rasulbekkomilov7@gmail.com>
2024-07-09 20:13:36 +0500

    qaytadan

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,6 @@ import NavbarSection from './components/NavbarSection.vue';
 import HeaderSection from './components/HeaderSection.vue';
 import Options from './components/Options.vue';
 import Card from './components/Cards.vue';
-import Footer from './components/Footer.vue';
-import Programms from './components/Programms.vue';
 </script>
 
 <template>
@@ -12,8 +10,6 @@ import Programms from './components/Programms.vue';
   <HeaderSection />
   <Options />
   <Card />
-  <Programms />
-  <Footer />
 </template>
 
 <style>
